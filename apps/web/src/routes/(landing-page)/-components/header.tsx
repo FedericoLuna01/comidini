819755328b90d076d42b658baa63bc0bd0138e9c
AlvaroReynoso <areynoso@frro.utn.edu.ar>
2025-07-10 00:12:19 -0300

    feat: implement AvatarDropdown component for user session management in header

--- apps/web/src/routes/(landing-page)/-components/header.tsx
+++ apps/web/src/routes/(landing-page)/-components/header.tsx
@@ -1,8 +1,20 @@
+import { authClient } from "@repo/auth/client";
+import { Avatar, AvatarFallback, AvatarImage } from "@repo/ui/components/ui/avatar";
 import { Button } from "@repo/ui/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@repo/ui/components/ui/dropdown-menu";
 import { Logo } from "@repo/ui/components/ui/logo";
 import { Link } from "@tanstack/react-router";
+import AvatarDropdown from "../../../components/avatar-dropdown";
 
 export const Header = () => {
+  const session = authClient.useSession();
+
   return (
     <header className="flex items-center justify-between container mx-auto py-4">
       <Logo />
@@ -23,14 +35,20 @@ export const Header = () => {
           Descargar
         </a>
       </nav>
-      <nav className="flex gap-4">
-        <Button asChild variant="secondary">
-          <Link to="/iniciar-sesion">Iniciar sesión</Link>
-        </Button>
-        <Button asChild variant="default">
-          <Link to="/registrarse">Registrarse</Link>
-        </Button>
-      </nav>
+      <div className="flex gap-4">
+        {session.data ? (
+          <AvatarDropdown />
+        ) : (
+          <>
+            <Button asChild variant="secondary">
+              <Link to="/iniciar-sesion">Iniciar sesión</Link>
+            </Button>
+            <Button asChild variant="default">
+              <Link to="/registrarse">Registrarse</Link>
+            </Button>
+          </>
+        )}
+      </div>
     </header>
   );
 };
